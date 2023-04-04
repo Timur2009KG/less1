3d338a9a58ee63e306b32347ce163f9225189195
Timur2009KG <asanalievt93@gmail.com>
2023-04-04 10:06:30 +0600

    first commit

---
+++ less1.py
@@ -0,0 +1,51 @@
+name = input('write your nick name:')
+
+print('when you go?')
+print('1 - go to race?')
+print('2 - go to lake?')
+print('3 - go to London?')
+print('4 - go to school?')
+print('5 - go to computer club')
+
+
+questions_1 = int(input ('your opinion? : '))
+
+
+if questions_1 == 1:
+    print('we go to race (: ')
+    print('1 - We walking? \n2 we play a ball')
+elif questions_1 == 2:
+    print('we go to lake')
+    print('3 - we we play footboal? \n4 we play volleyboal')
+elif questions_1 == 3: 
+    print('we go to london')
+    print('5 - we drink a coffe \n6 - we see a big ben!')
+elif questions_1 == 4:
+    print('4 - we go to school')
+    print('7 - we go to IT  class \n8 - we go to MATH')
+elif questions_1 == 5:
+    print('we dont go to computer club but we go to ACADEMY: (: ')
+    print('')
+else:
+    print('you write bed opinion. I kill you :)')
+
+questions_2 = int(input('what we doing? :'))
+
+if questions_2 == 1:
+    print('We walking')
+elif questions_2 == 2:
+    print('we play a ball')
+elif questions_2 == 3:
+    print('we play footboal')
+elif questions_2 == 4:
+    print('we play volleyboal')
+elif questions_2 == 5:
+    print('we drink a coffe!')
+elif questions_2 == 6:
+    print('we see a BIG Ben')
+elif questions_2 == 7:
+    print('we go to IT class')
+elif questions_2 == 8:
+    print('we go to IT class')
+
+
